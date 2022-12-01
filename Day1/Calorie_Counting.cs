d23e746a2e9145da87c4451ac1f2b4a7f51eccb9
Balaji Baskar <bbaskar@gmail.com>
2022-12-01 12:53:13 +0530

    Add project files.

---
+++ Day1/Calorie_Counting.cs
@@ -0,0 +1,22 @@
+﻿// See https://adventofcode.com/2022/day/1 for more information
+
+int counter = 0;
+List<int> calories = new();
+
+// Read the file and display it line by line.
+foreach (string line in System.IO.File.ReadLines(@"Day1/Input.txt"))
+{
+    string item = line.Trim();
+
+    if (item != string.Empty)
+    {
+        counter += Int32.Parse(item);
+    }
+    else
+    {
+        calories.Add(counter);
+        counter = 0;
+    }
+}
+
+Console.WriteLine("Elf carrying the most Calories (in total): " + calories.Max());
